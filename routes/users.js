7f051c46e706bd06342bee46dbeda0c8c61ddc68
bresha <hrvoje.bresic@gmail.com>
2020-04-10 20:41:53 +0200

    Finished users route

---
+++ routes/users.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const db = require('../config/db');
+
+router.post('/', async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json('Incorrect data');
+  }
+
+  let user = await db('users').where('username', username);
+  if (user.length > 0) {
+    return res.status(400).json('User already exists');
+  }
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const pwd_hash = bcrypt.hashSync(password, salt);
+    await db.transaction(async (trx) => {
+      const user = {
+        username,
+        pwd_hash,
+      };
+
+      const result = await db('users')
+        .returning('username')
+        .insert(user)
+        .transacting(trx);
+
+      res.json({ username: result[0], todos: [] });
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json('Server error');
+  }
+});
+
+router.get('/', async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json('Incorrect data');
+  }
+
+  let user = await db('users').where('username', username);
+  if (user.length == 0) {
+    return res.status(400).json('Wrong username');
+  }
+  const isValid = bcrypt.compareSync(password, user[0].pwd_hash);
+  if (isValid) {
+    // Get todos from db
+    const todos = await db('todos').where('user_id', user[0].user_id);
+    res.json({ username: user[0].username, todos: todos });
+  } else {
+    res.status(400).json('Wrong password');
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+// Middleware so we can read body form request
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('Helllo World!'));
+
+// Define routes
+app.use('/users', require('./routes/users'));
+
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
